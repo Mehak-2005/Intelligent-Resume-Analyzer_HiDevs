d322b2c0e1bae4669abc8b5ce46c35f66015d96f
Mehak-2005 <mehakm9620@gmail.com>
2026-03-18 11:38:22 +0530

    Add files via upload

---
+++ Smart Hiring Assistant/errors.py
@@ -0,0 +1,13 @@
+class ResumeAnalyzerError(Exception):
+    """Base exception for resume analyzer"""
+    pass
+
+
+class ParseError(ResumeAnalyzerError):
+    """Raised when parsing fails"""
+    pass
+
+
+class ValidationError(ResumeAnalyzerError):
+    """Raised when data validation fails"""
+    pass
